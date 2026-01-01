9045a3f2d9a533b555438bdaf7847303b14fd541
kunish <kunish.butt@gmail.com>
2026-01-02 02:19:31 +0800

    fix: ensure watch mode is disabled in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    watch: false,
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./test/setup.ts'],
